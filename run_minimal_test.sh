6970f15e8ec08c014f076e5582352cc66d3caccd
Hybris95 <hybris_95@hotmail.com>
2024-10-31 11:50:54 +0100

    dbg: Change how minimal test and uploading dump cores are done

--- run_minimal_test.sh
+++ run_minimal_test.sh
@@ -5,7 +5,4 @@ ulimit -c unlimited
 echo '/tmp/core.%e.%p' | sudo tee /proc/sys/kernel/core_pattern
 
 # Run tests with xvfb
-xvfb-run -a pytest -v test_minimal.py || true
-
-# List files in /tmp to check for core dumps
-ls -l /tmp
+xvfb-run -a pytest -v test_minimal.py
