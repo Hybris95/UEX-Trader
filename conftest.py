8bbba4f3476da7c836f993ff52053f9305c1fac3
Hybris95 <hybris_95@hotmail.com>
2024-12-20 18:03:05 +0100

    feat: Add Config tests for Version property and fix async issues loading config_manager specifically in tests

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ import asyncio
 from PyQt5.QtWidgets import QApplication
 import pytest_asyncio
 from gui import UexcorpTrader
-from config_manager import ConfigManager
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -13,11 +12,9 @@ async def qapp():
     app.quit()
 
 
-@pytest_asyncio.fixture(scope="session")
-async def config_manager():
-    config_manager = await ConfigManager.get_instance()
-    await config_manager.initialize()  # Ensure this is asynchronous
-    yield config_manager
+@pytest_asyncio.fixture()
+async def config_manager(trader):
+    yield trader.config_manager
 
 
 @pytest_asyncio.fixture
@@ -25,4 +22,3 @@ async def trader(qapp):
     trader = UexcorpTrader(qapp, asyncio.get_event_loop())
     await trader.initialize()
     yield trader
-    await trader.cleanup()
