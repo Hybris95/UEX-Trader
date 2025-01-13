0d5d4bf8c024e6b8f80ea08bd777af5878355bbc
Hybris95 <hybris_95@hotmail.com>
2025-01-13 11:38:42 +0100

    feat: Load single values from cache if available instead of filtering from big API results

--- global_variables.py
+++ global_variables.py
@@ -9,3 +9,8 @@ trade_route_tab_activated = True
 best_trade_route_tab_activated = True
 submit_tab_activated = False
 persistent_cache_activated = True
+
+# hard-coded entities TTL
+system_ttl = 86400
+planet_ttl = 86400
+terminal_ttl = 43200
